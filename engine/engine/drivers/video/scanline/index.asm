25789571c05a6a17b63a78a53e93ee3543637836
jameswilddev <jameswilddev@outlook.com>
2021-04-02 15:26:42 +0100

    Swap again.

--- engine/engine/drivers/video/scanline/index.asm
+++ engine/engine/drivers/video/scanline/index.asm
@@ -25,15 +25,15 @@ bst r30, 0
 brts video_scanline_before_columns_odd
 
 ; Even.
-lds r30, video_scanline_flags_a
-bst r30, VIDEO_SCANLINE_FLAG_PALETTE
-load_immediate_z video_scanline_buffer_a
-rjmp video_scanline_before_columns_end
-
-video_scanline_before_columns_odd:
 lds r30, video_scanline_flags_b
 bst r30, VIDEO_SCANLINE_FLAG_PALETTE
 load_immediate_z video_scanline_buffer_b
+rjmp video_scanline_before_columns_end
+
+video_scanline_before_columns_odd:
+lds r30, video_scanline_flags_a
+bst r30, VIDEO_SCANLINE_FLAG_PALETTE
+load_immediate_z video_scanline_buffer_a
 
 video_scanline_before_columns_end:
 .endm
