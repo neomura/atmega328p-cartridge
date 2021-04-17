5338a203b2ca09c34302b537136748ff00c41508
jameswilddev <jameswilddev@outlook.com>
2021-04-17 20:59:05 +0100

    Redesign the LUT helper.

--- engine/engine/utilities/declarations/lut.asm
+++ engine/engine/utilities/declarations/lut.asm
@@ -1,11 +1,11 @@
 ; Load a byte from a look-up table in flash in 8 cycles.
 ; Clobbers Z.
-; - A r* from which to read an offset into the look-up table.  The value will be written to this.
+; - A r* to which to write the looked-up byte.
+; - A r* from which to read an offset into the look-up table.  This can be the r* stored to.
 ; - The address of the start of the look-up table.
 .macro lut
-  load_immediate_z @1
-  add r30, @0
-  clr @0
-  adc r31, @0
+  load_immediate_z @2
+  add r30, @1
+  adc r31, zero
   lpm @0, Z
 .endm
